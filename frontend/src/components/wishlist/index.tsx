7610da7faa227aac39e16e8973d612331ea40c67
galbadrakh <galbadrak223@gmail.com>
2024-10-10 09:24:30 +0800

    no changes

---
+++ frontend/src/components/wishlist/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "../ui/card";
+import { Button } from "../ui/button";
+import { IoMdHeart } from "react-icons/io";
+
+const Wishlist = () => {
+  return (
+    <div className="my-5 ">
+      <Card className="w- h-[132px] m-auto bg-white dark:bg-black p-4 flex justify-between">
+        <div className="flex gap-6">
+          <img src="\images\girl.png" alt="" className="rounded-xl" />
+          <div className="flex flex-col justify-between">
+            <p className="text-base">Chunky Glyph Tee</p>
+            <p className="text-sm font-bold">120’000₮</p>
+            <Button
+              variant={"outline"}
+              className="bg-[#2563EB] text-white text-sm w-20"
+              size="sm"
+            >
+              Сагслах
+            </Button>
+          </div>
+        </div>
+        <IoMdHeart className=" text-2xl" />
+      </Card>
+    </div>
+  );
+};
+
+export default Wishlist;
